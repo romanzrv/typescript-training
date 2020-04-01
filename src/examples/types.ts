b1abb3f46f5b12a56c98edeabe7dffffbfcb7f73
Roman <roman.zukov@lifullconnect.com>
2020-04-01 13:01:33 +0200

    Types examples

---
+++ src/examples/types.ts
@@ -0,0 +1,15 @@
+// Variables types
+
+let text: string = 'Some text here';
+
+// Function arguments types
+
+function foo1(name: string) {
+    return `Hello, ${name}`;
+}
+
+// Function returning value types
+
+function foo2(name: string): string {
+    return `Hello, ${name}`;
+}
