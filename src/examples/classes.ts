5543ee0b315783dff27b2daa09c07d2dd64393c1
Roman <roman.zukov@lifullconnect.com>
2020-04-01 13:44:16 +0200

    Classes examples

---
+++ src/examples/classes.ts
@@ -0,0 +1,15 @@
+class Person {
+    private fullName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.fullName = `${firstName} ${lastName}`;
+    }
+
+    public getFullName(): string {
+        return this.fullName;
+    }
+
+    public setFullName(firstName: string, lastName: string) {
+        this.fullName = `${firstName} ${lastName}`;
+    }
+}
